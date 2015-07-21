4a0eee93f01e15a573296900f9d5a8ecdfcf4360
altugyigit <altug_yigit@hotmail.com>
2015-07-21 14:07:45 +0300

    hanimeli dataseti zor olduğu için menekşe, papatya, lale dataseti hazırlandı fakat başarısızlıklar oldu bu yüzden maske çıkarılması gerekiyor.

--- TacTest.py
+++ TacTest.py
@@ -8,13 +8,13 @@ import cv2
 import glob
 
 #****************************************************TAC EGITIM***************************************************************
-imagePaths = sorted(glob.glob("dataset/hanimeli" + "/*.jpg"))
+imagePaths = sorted(glob.glob("dataset/imagesjpg" + "/*.jpg"))
 maskPaths = sorted(glob.glob("dataset/masks" + "/*.png"))
 
 trainObj = TrainClassify(imagePaths, maskPaths)
 
 #****************************************************TAC TEST***************************************************************
-imagePath = 'testimgh.jpg'
+imagePath = 'test/testimglale4.jpg'
 image = cv2.imread(imagePath)
 
 rgbHistObj = RGBHistogram([8, 8, 8])
@@ -29,4 +29,4 @@ if flower == 'daisy': flower = 'papatya'
 if flower == 'pansy' : flower = 'menekse'
 if flower == 'sunflower' : flower = 'aycicegi'
 
-print "Bu cicek %s olabilir." % (flower.upper())
+print "Bu cicek %s cinsi olabilir." % (flower.upper())
