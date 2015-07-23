c21fa85f57ce72e1eb22bc2b9fcee50d7ce81988
altugyigit <altug_yigit@hotmail.com>
2015-07-23 23:22:01 +0300

    Otsu

--- GaussianBlurExample.py
+++ GaussianBlurExample.py
@@ -1,11 +1,25 @@
+import mahotas as mahotas
+
 __author__ = 'altug'
 
 import numpy as np
 import argparse
 import cv2
 
-image = cv2.imread('test/testimgh.jpg')
+image = cv2.imread('test/testimg.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(image, (5, 5), 0)
-cv2.imshow("Image", image)
+cv2.imshow("Image", blurred)
+
+T = mahotas.thresholding.otsu(blurred)
+print "Otsu degeri: %d" % (T)
+
+thresh = image.copy()
+thresh[thresh > T] = 255
+thresh[thresh < 255] = 0
+
+thresh = cv2.bitwise_not(thresh)
+cv2.imshow("Otsu", thresh)
+
+
 cv2.waitKey()
